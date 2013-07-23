317e1bafa5784fb95ce0b5670a114d5b7795d1ae
Gavin Price <gaprice@lbl.gov>
2013-07-23 16:07:12 -0700

    Test works

--- src/us/kbase/shock/client/test/ShockTests.java
+++ src/us/kbase/shock/client/test/ShockTests.java
@@ -1,9 +1,16 @@
 package us.kbase.shock.client.test;
 
+import java.net.URL;
+
+import junit.framework.Assert;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import us.kbase.auth.AuthService;
+import us.kbase.auth.AuthToken;
 import us.kbase.shock.client.BasicShockClient;
+import us.kbase.shock.client.ShockNode;
 
 public class ShockTests {
 	
@@ -12,16 +19,24 @@ public class ShockTests {
 	private static BasicShockClient bscNoAuth;
 	
 	@BeforeClass
-	public static void setUpClass() {
-		String shockurl = System.getProperty("test.shock.url");
-		System.out.println(shockurl);
-		System.out.println(System.getProperty("test.user1"));
-		System.out.println(System.getProperty("test.pwd1"));
-		System.out.println(System.getProperty("test.user2"));
-		System.out.println(System.getProperty("test.pwd2"));
-		
+	public static void setUpClass() throws Exception {
+		URL url = new URL(System.getProperty("test.shock.url"));
+		System.out.println("Testing shock clients pointed at: " + url);
+		String u1 = System.getProperty("test.user1");
+		String u2 = System.getProperty("test.user2");
+		String p1 = System.getProperty("test.pwd1");
+		String p2 = System.getProperty("test.pwd2");
+		AuthToken t1 = AuthService.login(u1, p1).getToken();
+		AuthToken t2 = AuthService.login(u2, p2).getToken();
+		bsc1 = new BasicShockClient(url, t1);
+		bsc2 = new BasicShockClient(url, t2);
+		bscNoAuth = new BasicShockClient(url);
 	}
 
 	@Test
-	public void passing() {}
+	public void getNodeBasic() throws Exception {
+		ShockNode sn = bsc1.addNode();
+		ShockNode snget = bsc1.getNode(sn.getId());
+		Assert.assertEquals("get node != add Node output", sn.toString(), snget.toString());
+	}
 }
