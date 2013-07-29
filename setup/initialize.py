08518f880f174b18aa1d457ff955f9a431c19643
Gavin Price <gaprice@lbl.gov>
2013-07-29 14:12:20 -0700

    better config parsing, connect to mongo

--- setup/initialize.py
+++ setup/initialize.py
@@ -5,22 +5,33 @@
 from __future__ import print_function
 import os
 import sys
-from ConfigParser import ConfigParser
-import configobj
+from configobj import ConfigObj
+from pymongo import MongoClient
 
 try:  # py 2/3 compatibility
-    input = raw_input
+    input = raw_input  # @ReservedAssignment
 except NameError:
     pass
 
 __AUTHOR__ = 'gaprice@lbl.gov'
 
-configfile = 'deploy.cfg'
-configheader = 'Workspace'
+CONFIGFILE = 'deploy.cfg'
+CONFIGHEADER = 'Workspace'
 
-moduledir = 'workspace_deluxe'
-setupdir = 'setup'
-filename = 'initialize.py'
+MODULEDIR = 'workspace_deluxe'
+SETUPDIR = 'setup'
+FILENAME = 'initialize.py'
+
+MONGO = 'mongodb-'
+
+MODB = MONGO + 'database'
+MOHOST = MONGO + 'host'
+MOUSER = MONGO + 'user'
+MOPWD = MONGO + 'pwd'
+
+
+REQPARAMS = [MOHOST, MODB]
+AUTHPARAMS = [MOUSER, MOPWD]
 
 
 def printerr(*objs):
@@ -30,11 +41,11 @@ def printerr(*objs):
 
 def renamederr(err):
     printerr(err + ' Please use the {} program found in the {} folder.'.format(
-                filename, os.path.join(moduledir, setupdir)))
+                FILENAME, os.path.join(MODULEDIR, SETUPDIR)))
 
 
 def printcfg(cfg, header):
-    for k, v in cfg.items(header):
+    for k, v in cfg[header].iteritems():
         print(k + '=' + v)
 
 
@@ -67,34 +78,69 @@ def getinput(prompt, default=(), otheroptions={}, quitopt=False):
                 return i
 
 
+def getparams(params, cfg, dropall):
+    for key in params:
+        if dropall or key not in cfg:
+            cfg[key] = input("Please enter value for " + key + ": ")
+
+
 if __name__ == '__main__':
     d, program = os.path.split(os.path.abspath(__file__))
     wd, setup = os.path.split(d)
-    if os.path.basename(wd) != moduledir or setup != setupdir:
+    if os.path.basename(wd) != MODULEDIR or setup != SETUPDIR:
         renamederr('This program has been moved from its standard location.')
-    if program != filename:
+    if program != FILENAME:
         renamederr('This program has been renamed.')
-    cfgfile = os.path.join(wd, configfile)
+    cfgfile = os.path.join(wd, CONFIGFILE)
     if os.path.isdir(cfgfile):
         printerr(cfgfile + ' is a directory!')
-    cfg = ConfigParser()
-    keepconfig = False
+    cfg = ConfigObj()
+    dropconfig = False
     if os.path.isfile(cfgfile):
-        cfg.read(cfgfile)
-        if not cfg.has_section(configheader):
+        cfg = ConfigObj(cfgfile)
+        if CONFIGHEADER not in cfg:
             print('No {} section in config file {} - '.format(
-                configheader, cfgfile) + 'will be created.')
-            cfg.add_section(configheader)
+                CONFIGHEADER, cfgfile) + 'will be created.')
+            cfg[CONFIGHEADER] = None
         else:
             print('Current configuration:')
-            printcfg(cfg, configheader)
-            keepconfig = getinput('Keep this configuration?', ('y', 'keep'),
-                                  {'n': 'discard'}) == 'y'
-            if keepconfig:
-                print('Keeping current configuration.')
-            else:
+            printcfg(cfg, CONFIGHEADER)
+            dropconfig = getinput('Keep this configuration?', ('y', 'keep'),
+                                  {'n': 'discard'}) == 'n'
+            if dropconfig:
                 print('Discarding current configuration.')
-    # use configobj instead
+            else:
+                print('Keeping current configuration.')
+    wscfg = cfg[CONFIGHEADER]
+    getparams(REQPARAMS, wscfg, dropconfig)
+    authrqd = getinput('Does mongodb require authentication?', ('y', 'yes'),
+                {'n': 'no'}) == 'y'
+    if authrqd:
+        getparams(AUTHPARAMS, wscfg, dropconfig)
+    else:
+        print('Ok, commenting out authorization information.')
+        for key in AUTHPARAMS:
+            if key in wscfg:
+                wscfg['#' + key] = wscfg[key]
+                del wscfg[key]
+    print('Attempting to connect to mongodb database "' + wscfg[MODB] +
+          '" at ' + wscfg[MOHOST] + '... ', end='')
+    try:
+        mongo = MongoClient(wscfg[MOHOST])
+    except:
+        printerr('\nCould not connect to mongodb at ' + wscfg[MOHOST])
+    db = mongo[wscfg[MODB]]
+    if authrqd:
+        try:
+            db.authenticate(wscfg[MOUSER], wscfg[MOPWD])
+        except:
+            printerr('\nUnable to authenticate to database "' + wscfg[MODB] +
+                     '"')
+    print('Connected.')
+    print(db)
+    
+    
+    print(cfg)
+    
             
-        
         
