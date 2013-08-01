551d28e0f6b9b1b12f403668a4924e0dc32c6998
Gavin Price <gaprice@lbl.gov>
2013-08-01 14:42:51 -0700

    Add gridFS/shock indicator and shock user to the DB config

--- setup/initialize.py
+++ setup/initialize.py
@@ -9,6 +9,8 @@ from configobj import ConfigObj
 from pymongo import MongoClient
 import urllib2
 import json
+import httplib2
+import base64
 
 try:  # py 2/3 compatibility
     input = raw_input  # @ReservedAssignment
@@ -33,7 +35,11 @@ MOPWD = MONGO + 'pwd'
 
 SETTINGS = 'settings'
 SHOCKURL = 'shock_location'
-
+SHOCKUSER = 'shock_user'
+BACKEND = 'backend'
+BACKENDCREDS = 'backend_secret'
+SHOCK = 'shock'
+GFS = 'gridFS'
 
 REQPARAMS = [MOHOST, MODB]
 AUTHPARAMS = [MOUSER, MOPWD]
@@ -89,22 +95,69 @@ def getparams(params, cfg, dropall):
             cfg[key] = input('Please enter value for ' + key + ': ')
 
 
-def setshockurlfromuser():
-    shockurl = input('Please enter the url of the shock server: ')
-    try:
-        r = urllib2.urlopen(shockurl).read()
-    except:
-        printerr("Couldn't contact the shock server at " + shockurl)
-    try:
-        j = json.loads(r)
-    except:
-        printerr(shockurl + ' is not a shock server root url')
-    if 'id' not in j or j['id'] != 'Shock':
-        printerr(shockurl + ' is not a shock server root url')
-    shockurl = j['url']
-    db[SETTINGS].update({}, {'$set': {SHOCKURL: shockurl}}, upsert=True)
-    shock = db[SETTINGS].find_one()
-    print('Successfully set shock location to ' + shock[SHOCKURL])
+def printDBsettings(settings):
+    for s in [BACKEND, SHOCKURL, SHOCKUSER]:
+        print(s + '=' + str(settings.get(s, None)))
+
+
+# TODO replace this with the py auth lib when it's ready
+def _get_token(user_id, password,
+        auth_svc='https://nexus.api.globusonline.org/goauth/token?' +
+            'grant_type=client_credentials'):
+    h = httplib2.Http(disable_ssl_certificate_validation=True)
+
+    auth = base64.encodestring(user_id + ':' + password)
+    headers = {'Authorization': 'Basic ' + auth}
+
+    h.add_credentials(user_id, password)
+    h.follow_all_redirects = True
+    url = auth_svc
+
+    resp, content = h.request(url, 'GET', headers=headers)
+    status = int(resp['status'])
+    if status >= 200 and status <= 299:
+        tok = json.loads(content)
+    elif status == 403:
+        raise Exception('Authentication failed: Bad user_id/password ' +
+                        'combination %s:%s' % (user_id, password))
+    else:
+        raise Exception(str(resp))
+    return tok['access_token']
+
+
+def configDB(wscfg):
+    settings = {SHOCKURL: None, SHOCKUSER: None, BACKEND: None}
+    backend = getinput('Choose a backend: ', ('s', SHOCK), {'g': GFS})
+    if backend == 's':
+        settings[BACKEND] = SHOCK
+        shockurl = input('Please enter the url of the shock server: ')
+        shockuser = input('Please enter the workspace shock username: ')
+        shockpwd = input('Please enter the workspace shock password: ')
+        try:
+            _get_token(shockuser, shockpwd)
+        except:
+            printerr('Authorization for user {} failed.'.format(shockuser))
+        wscfg[BACKENDCREDS] = shockpwd
+        try:
+            r = urllib2.urlopen(shockurl).read()
+        except:
+            printerr("Couldn't contact the shock server at " + shockurl)
+        try:
+            j = json.loads(r)
+        except:
+            printerr(shockurl + ' is not a shock server root url')
+        if 'id' not in j or j['id'] != 'Shock':
+            printerr(shockurl + ' is not a shock server root url')
+        settings[SHOCKURL] = j['url']
+        settings[SHOCKUSER] = shockuser
+    else:
+        settings[BACKEND] = GFS
+
+    db[SETTINGS].update({}, settings, upsert=True)
+    settings = db[SETTINGS].find_one()
+    print('Successfully set DB configuration:')
+    printDBsettings(settings)
+    print()
 
 
 if __name__ == '__main__':
@@ -126,14 +179,14 @@ if __name__ == '__main__':
                 CONFIGHEADER, cfgfile) + 'will be created.')
             cfg[CONFIGHEADER] = {}
         else:
-            print('Current configuration:')
+            print('Current configuration file:')
             printcfg(cfg, CONFIGHEADER)
             dropconfig = getinput('\nKeep this configuration?', ('y', 'keep'),
                                   {'n': 'discard'}) == 'n'
             if dropconfig:
-                print('Discarding current configuration.')
+                print('Discarding current local configuration.')
             else:
-                print('Keeping current configuration.')
+                print('Keeping current local configuration.')
     wscfg = cfg[CONFIGHEADER]
     getparams(REQPARAMS, wscfg, dropconfig)
     authrqd = getinput('Does mongodb require authentication?', ('y', 'yes'),
@@ -160,21 +213,24 @@ if __name__ == '__main__':
             printerr('\nUnable to authenticate to database "' + wscfg[MODB] +
                      '"')
     print('Connected.')
-    cfg.write()
-    print('Configuration saved.')
-    shock = None
+    settings = None
     if SETTINGS in db.collection_names():
-        shock = db[SETTINGS].find_one()
-    if shock != None:
-        shockurl = shock[SHOCKURL]
-        print('The database is already initialized with the shock server ' +
-              'at:\n' + shockurl)
+        settings = db[SETTINGS].find_one()
+    if settings != None:
+#        shockurl = shock.get(SHOCKURL, None)
+#        shockuser = settings.get(SHOCKUSER, None)
+#        backend = settings.get(BACKEND, None)
+        print('The database is already initialized with the parameters:')
+        printDBsettings(settings)
+#        for s in [BACKEND, SHOCKURL, SHOCKUSER]:
+#            print(s + '=' + settings.get(s, None))
         print(
-'''You can change the shock url now, but if the workspace service has already
-saved objects it will put the workspace and shock in inconsistent states,
-all workspace objects will be irretrievable, and you will make a lot of people
-really really mad.''')
-        prompts = ['Do you want to change the shock url?',
+'''\nYou can change the server configuration now, but if the workspaceservice
+has already saved objects it could put the workspace and the backend store
+(gridFS or shock) in an inconsistent state, in which case all workspace
+objects will be irretrievable and you will make a lot of people really really
+mad.''')
+        prompts = ['Do you want to change the configuration?',
                    'Are you absolutely sure you know what you are ' +
                    'doing?\nThe consequences are dire, dire!',
                    'Seriously? You are about to royally jack ' +
@@ -186,7 +242,10 @@ really really mad.''')
                 break
             if n == len(prompts) - 1:
                 print('Ok, on your head be it.')
-                setshockurlfromuser()
+                configDB(wscfg)
     else:
-        setshockurlfromuser()
-    print('\nConfiguration complete.')
+        configDB(wscfg)
+    print('Saving local configuration file:')
+    printcfg(cfg, CONFIGHEADER)
+    cfg.write()
+    print('\nConfiguration saved.')
