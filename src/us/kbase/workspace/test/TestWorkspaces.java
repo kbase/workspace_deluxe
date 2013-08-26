4e296f32e755f5c25717b096d0b3e30d3554be35
Gavin Price <gaprice@lbl.gov>
2013-08-26 14:21:26 -0700

    Test ws description & bad input to create_ws

--- src/us/kbase/workspace/test/TestWorkspaces.java
+++ src/us/kbase/workspace/test/TestWorkspaces.java
@@ -1,9 +1,14 @@
 package us.kbase.workspace.test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +16,11 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import us.kbase.workspace.database.Database;
+import us.kbase.workspace.database.exceptions.NoSuchWorkspaceException;
+import us.kbase.workspace.database.exceptions.PreExistingWorkspaceException;
 import us.kbase.workspace.database.mongo.MongoDatabase;
+import us.kbase.workspace.exceptions.WorkspaceAuthorizationException;
+import us.kbase.workspace.workspaces.WorkspaceIdentifier;
 import us.kbase.workspace.workspaces.Workspaces;
 
 import com.mongodb.BasicDBObject;
@@ -26,6 +35,8 @@ public class TestWorkspaces {
 	public static final String M_USER = "test.mongo.user";
 	public static final String M_PWD = "test.mongo.pwd";
 	public static Workspaces[] TEST_WORKSPACES = new Workspaces[2];
+	public static final String LONG_TEXT_PART = "Passersby were amazed by the unusually large amounts of blood. ";
+	public static String LONG_TEXT = "";
 
 	@Parameters
 	public static Collection<Object[]> generateData() throws Exception {
@@ -74,6 +85,8 @@ public class TestWorkspaces {
 		}
 		DBObject dbo = new BasicDBObject();
 		mdb.getCollection("settings").remove(dbo);
+		mdb.getCollection("workspaces").remove(dbo);
+		mdb.getCollection("workspaceACLs").remove(dbo);
 		dbo.put("backend", "gridFS");
 		mdb.getCollection("settings").insert(dbo);
 		Database db = null;
@@ -92,6 +105,8 @@ public class TestWorkspaces {
 		}
 		dbo = new BasicDBObject();
 		mdb.getCollection("settings").remove(dbo);
+		mdb.getCollection("workspaces").remove(dbo);
+		mdb.getCollection("workspaceACLs").remove(dbo);
 		dbo.put("backend", "shock");
 		dbo.put("shock_user", shockuser);
 		dbo.put("shock_location", shockurl);
@@ -103,15 +118,100 @@ public class TestWorkspaces {
 		}
 		TEST_WORKSPACES[1] = new Workspaces(db);
 		assertTrue("Shock backend setup failed", TEST_WORKSPACES[1].getBackendType().equals("Shock"));
+		for (int i = 0; i < 17; i++) {
+			LONG_TEXT += LONG_TEXT_PART;
+		}
 	}
 	
 	public TestWorkspaces(Workspaces ws) {
 		this.ws = ws;
 	}
 	
+	@Test
+	public void testWorkspaceDescription() throws PreExistingWorkspaceException,
+			NoSuchWorkspaceException, WorkspaceAuthorizationException {
+		ws.createWorkspace("auser", "lt", false, LONG_TEXT);
+		ws.createWorkspace("auser", "ltp", false, LONG_TEXT_PART);
+		String desc = ws.getWorkspaceDescription("auser", new WorkspaceIdentifier("lt"));
+		assertThat("Workspace description incorrect", desc, is(LONG_TEXT.substring(0, 1000)));
+		desc = ws.getWorkspaceDescription("auser", new WorkspaceIdentifier("ltp"));
+		assertThat("Workspace description incorrect", desc, is(LONG_TEXT_PART));
+	}
 	
 	@Test
-	public void fatDumbAndHappy() {
-		System.out.println(ws.getBackendType());
+	public void testCreateWorkspaceAndGetMeta() {
+//		ws.createWorkspace("auser", "foo", globalread, description)
+	}
+	
+	@Test
+	public void testCreateWorkspaceAndWorkspaceIdentifierWithBadInput()
+			throws PreExistingWorkspaceException {
+		List<List<String>> userWS = new ArrayList<List<String>>();
+		//test a few funny chars in the ws name
+		userWS.add(Arrays.asList("afaeaafe", "afe_aff*afea",
+				"Illegal character in workspace name afe_aff*afea: *"));
+		userWS.add(Arrays.asList("afaeaafe", "afeaff/af*ea",
+				"Illegal character in workspace name afeaff/af*ea: /"));
+		userWS.add(Arrays.asList("afaeaafe", "af?eaff*afea",
+				"Illegal character in workspace name af?eaff*afea: ?"));
+		//check missing ws name
+		userWS.add(Arrays.asList("afaeaafe", null,
+				"name cannot be null and must have at least one character"));
+		userWS.add(Arrays.asList("afaeaafe", "",
+				"name cannot be null and must have at least one character"));
+		//check missing user and/or workspace name in compound name
+		userWS.add(Arrays.asList("afaeaafe", ":",
+				"Workspace name missing from :"));
+		userWS.add(Arrays.asList("afaeaafe", "foo:",
+				"Workspace name missing from foo:"));
+		userWS.add(Arrays.asList("afaeaafe", ":foo",
+				"User name missing from :foo"));
+		//check multiple delims
+		userWS.add(Arrays.asList("afaeaafe", "foo:a:foo",
+				"Workspace name foo:a:foo may only contain one : delimiter"));
+		userWS.add(Arrays.asList("afaeaafe", "foo::foo",
+				"Workspace name foo::foo may only contain one : delimiter"));
+		
+		for (List<String> testdata: userWS) {
+			String wksps = testdata.get(1);
+			try {
+				new WorkspaceIdentifier(wksps);
+				fail(String.format("able to create workspace identifier with illegal input ws %s",
+						wksps));
+			} catch (IllegalArgumentException e) {
+				assertThat("incorrect exception message", e.getLocalizedMessage(),
+						is(testdata.get(2)));
+			}
+		}
+		
+		//check missing user name
+		userWS.add(Arrays.asList(null, "foo",
+				"user cannot be null and must have at least one character"));
+		userWS.add(Arrays.asList("", "foo",
+				"user cannot be null and must have at least one character"));
+		//user must match prefix
+		userWS.add(Arrays.asList("auser", "notauser:foo", 
+				"Workspace name notauser:foo must only contain the user name auser prior to the : delimiter"));
+		
+		for (List<String> testdata: userWS) {
+			String user = testdata.get(0);
+			String wksps = testdata.get(1);
+			try {
+				ws.createWorkspace(user, wksps, false, "iswaffertheen");
+				fail(String.format("able to create workspace with illegal input user: %s ws %s",
+						user, wksps));
+			} catch (IllegalArgumentException e) {
+				assertThat("incorrect exception message", e.getLocalizedMessage(),
+						is(testdata.get(2)));
+			}
+			try {
+				new WorkspaceIdentifier(wksps, user);
+				fail(String.format("able to create workspace identifier with illegal input user: %s ws %s",
+						user, wksps));
+			} catch (IllegalArgumentException e) {
+				assertThat("incorrect exception message", e.getLocalizedMessage(),
+						is(testdata.get(2)));
+			}
+		}
 	}
 }
