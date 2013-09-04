9449589ee3f59815071a7c059e731b3115de34f4
Gavin Price <gaprice@lbl.gov>
2013-09-03 17:59:48 -0700

    Start refactoring test setup

---
+++ src/us/kbase/workspace/test/Common.java
@@ -0,0 +1,141 @@
+package us.kbase.workspace.test;
+
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+
+import us.kbase.workspace.database.exceptions.InvalidHostException;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+
+public class Common {
+	
+	public static final String DB1 = "test.mongo.db1";
+	public static final String DB2 = "test.mongo.db2";
+	public static final String HOST = "test.mongo.host";
+	public static final String M_USER = "test.mongo.user";
+	public static final String M_PWD = "test.mongo.pwd";
+	public static final String SHOCKURL = "test.shock.url";
+	public static final String GRIDFS = "gridFS";
+	public static final String SHOCK = "shock";
+	public static final List<String> COLLECTIONS = Arrays.asList(
+			"settings", "workspaces", "workspaceACLs", "workspaceCounter");
+			
+	private static MongoClient mongoClient = null;
+	
+	public static void printJava() {
+		System.out.println("Java: " + System.getProperty("java.runtime.version"));
+	}
+	
+	private static String getProp(String prop) {
+		if (System.getProperty(prop) == null) {
+			throw new TestException("Property " + prop + " cannot be null.");
+		}
+		return System.getProperty(prop);
+	}
+	
+	public static String getHost() {
+		return getProp(HOST);
+	}
+	
+	public static String getMongoUser() {
+		return System.getProperty(M_USER);
+	}
+	
+	public static String getMongoPwd() {
+		return System.getProperty(M_PWD);
+	}
+	
+	public static String getDB1() {
+		return getProp(DB1);
+	}
+	
+	public static String getDB2() {
+		return getProp(DB2);
+	}
+	
+	public static String getShockUrl() {
+		return getProp(SHOCKURL);
+	}
+	
+	private static void buildMongo() throws UnknownHostException,
+			InvalidHostException, TestException {
+		if (mongoClient != null) {
+			return;
+		}
+		printJava();
+		String host = getHost();
+		String mUser = getMongoUser();
+		String mPwd = getMongoPwd();
+		if (mUser.equals("")) {
+			mUser = null;
+		}
+		if (mPwd.equals("")) {
+			mPwd = null;
+		}
+		if (mUser == null ^ mPwd == null) {
+			System.err.println(String.format("Must provide both %s and %s ",
+					M_USER, M_PWD) + "params for testing if authentication " + 
+					"is to be used");
+			System.exit(1);
+		}
+		System.out.print("Mongo auth params are user: " + mUser + " pwd: ");
+		if (mPwd != null && mPwd.length() > 0) {
+			System.out.println("[redacted]");
+		} else {
+			System.out.println(mPwd);
+		}
+		if (mongoClient == null) {
+			final MongoClientOptions opts = MongoClientOptions.builder()
+					.autoConnectRetry(true).build();
+			try {
+				mongoClient = new MongoClient(host, opts);
+			} catch (NumberFormatException nfe) {
+				throw new InvalidHostException(host
+						+ " is not a valid mongodb host");
+			}
+		}
+	}
+	
+//		String shockurl = System.getProperty("test.shock.url");
+//		String shockuser = System.getProperty("test.user.noemail");
+//		String shockpwd = System.getProperty("test.pwd.noemail");
+	
+	//run this method first, lots of error checking
+	public static void destroyAndSetupDB(int num, String type, String shockuser)
+			throws InvalidHostException, UnknownHostException, TestException {
+		buildMongo();
+		String dbname = num == 1 ? DB1 : DB2;
+		String db = System.getProperty(dbname);
+		if (db == null) {
+			throw new TestException("The property " + dbname + " is not set.");
+		}
+		String mUser = getMongoUser();
+		String mPwd = getMongoPwd();
+		DB mdb = mongoClient.getDB(db);
+		if (mUser != null) {
+			mdb.authenticate(mUser, mPwd.toCharArray());
+		}
+		DBObject dbo = new BasicDBObject();
+		for (String c: COLLECTIONS) {
+			mdb.getCollection(c).remove(dbo);
+		}
+		if (type == GRIDFS) {
+			dbo.put("backend", GRIDFS);
+		}
+		if (type == SHOCK) {
+			dbo.put("backend", SHOCK);
+			if (shockuser == null) {
+				throw new TestException("Shock user cannot be null");
+			}
+			dbo.put("shock_user", shockuser);
+			dbo.put("shock_location", getShockUrl());
+		}
+		mdb.getCollection("settings").insert(dbo);
+	}
+}
--- src/us/kbase/workspace/workspaces/test/TestWorkspaces.java
+++ src/us/kbase/workspace/workspaces/test/TestWorkspaces.java
@@ -23,22 +23,16 @@ import us.kbase.workspace.database.Database;
 import us.kbase.workspace.database.exceptions.PreExistingWorkspaceException;
 import us.kbase.workspace.database.mongo.MongoDatabase;
 import us.kbase.workspace.exceptions.WorkspaceAuthorizationException;
+import us.kbase.workspace.test.Common;
 import us.kbase.workspace.workspaces.Permission;
 import us.kbase.workspace.workspaces.WorkspaceIdentifier;
 import us.kbase.workspace.workspaces.WorkspaceMetaData;
 import us.kbase.workspace.workspaces.Workspaces;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-
 //TODO test vs. auth'd mongo
 @RunWith(Parameterized.class)
 public class TestWorkspaces {
 
-	public static final String M_USER = "test.mongo.user";
-	public static final String M_PWD = "test.mongo.pwd";
 	public static Workspaces[] TEST_WORKSPACES = new Workspaces[2];
 	public static final String LONG_TEXT_PART = "Passersby were amazed by the unusually large amounts of blood. ";
 	public static String LONG_TEXT = "";
@@ -55,76 +49,30 @@ public class TestWorkspaces {
 	public final Workspaces ws;
 	
 	public static void setUpWorkspaces() throws Exception {
-		System.out.println("Java: " + System.getProperty("java.runtime.version"));
-		String host = System.getProperty("test.mongo.host");
-		String db1 = System.getProperty("test.mongo.db1");
-		String db2 = System.getProperty("test.mongo.db2");
-		String mUser = System.getProperty(M_USER);
-		String mPwd = System.getProperty(M_PWD);
-		String shockurl = System.getProperty("test.shock.url");
 		String shockuser = System.getProperty("test.user.noemail");
 		String shockpwd = System.getProperty("test.pwd.noemail");
+		Common.destroyAndSetupDB(1, "gridFS", null);
+		Common.destroyAndSetupDB(2, "shock", shockuser);
+		String host = Common.getHost();
+		String mUser = Common.getMongoUser();
+		String mPwd = Common.getMongoPwd();
+		String db1 = Common.getDB1();
+		String db2 = Common.getDB2();
 		
-		if (mUser.equals("")) {
-			mUser = null;
-		}
-		if (mPwd.equals("")) {
-			mPwd = null;
-		}
-		if (mUser == null ^ mPwd == null) {
-			System.err.println(String.format("Must provide both %s and %s ",
-					M_USER, M_PWD) + "params for testing if authentication " + 
-					"is to be used");
-			System.exit(1);
-		}
-		System.out.print("Mongo auth params are user: " + mUser + " pwd: ");
-		if (mPwd != null && mPwd.length() > 0) {
-			System.out.println("[redacted]");
-		} else {
-			System.out.println(mPwd);
-		}
-		//Set up mongo backend database
-		DB mdb = new MongoClient(host).getDB(db1);
+		Database gfs = null;
+		Database shock = null;
 		if (mUser != null) {
-			mdb.authenticate(mUser, mPwd.toCharArray());
-		}
-		DBObject dbo = new BasicDBObject();
-		mdb.getCollection("settings").remove(dbo);
-		mdb.getCollection("workspaces").remove(dbo);
-		mdb.getCollection("workspaceACLs").remove(dbo);
-		mdb.getCollection("workspaceCounter").remove(dbo);
-		dbo.put("backend", "gridFS");
-		mdb.getCollection("settings").insert(dbo);
-		Database db = null;
-		if (mUser != null) {
-			db = new MongoDatabase(host, db1, shockpwd, mUser, mPwd);
+			gfs = new MongoDatabase(host, db1, shockpwd, mUser, mPwd);
+			shock = new MongoDatabase(host, db2, shockpwd, mUser, mPwd);
 		} else {
-			db = new MongoDatabase(host, db1, shockpwd);
+			gfs = new MongoDatabase(host, db1, shockpwd);
+			shock = new MongoDatabase(host, db2, shockpwd);
 		}
-		TEST_WORKSPACES[0] = new Workspaces(db);
+		TEST_WORKSPACES[0] = new Workspaces(gfs);
 		assertTrue("GridFS backend setup failed", TEST_WORKSPACES[0].getBackendType().equals("GridFS"));
-		
-		//Set up shock backend database
-		mdb = new MongoClient(host).getDB(db2);
-		if (mUser != null) {
-			mdb.authenticate(mUser, mPwd.toCharArray());
-		}
-		dbo = new BasicDBObject();
-		mdb.getCollection("settings").remove(dbo);
-		mdb.getCollection("workspaces").remove(dbo);
-		mdb.getCollection("workspaceACLs").remove(dbo);
-		mdb.getCollection("workspaceCounter").remove(dbo);
-		dbo.put("backend", "shock");
-		dbo.put("shock_user", shockuser);
-		dbo.put("shock_location", shockurl);
-		mdb.getCollection("settings").insert(dbo);
-		if (mUser != null) {
-			db = new MongoDatabase(host, db2, shockpwd, mUser, mPwd);
-		} else {
-			db = new MongoDatabase(host, db2, shockpwd);
-		}
-		TEST_WORKSPACES[1] = new Workspaces(db);
+		TEST_WORKSPACES[1] = new Workspaces(shock);
 		assertTrue("Shock backend setup failed", TEST_WORKSPACES[1].getBackendType().equals("Shock"));
+		
 		for (int i = 0; i < 17; i++) {
 			LONG_TEXT += LONG_TEXT_PART;
 		}
