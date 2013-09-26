cb9814dba58eb3305462971501e74c53cea91794
msneddon <mwsneddon@lbl.gov>
2013-09-26 15:00:30 -0700

    updated some id_ref validation tests

--- src/us/kbase/typedobj/tests/TestIdProcessing.java
+++ src/us/kbase/typedobj/tests/TestIdProcessing.java
@@ -155,7 +155,7 @@ public class TestIdProcessing {
 		//	db.releaseType("FBA", typename, username);
 		//}
 		
-		System.out.println("finding test instances");
+		System.out.println("finding test instances\n");
 		String [] resources = getResourceListing(TEST_RESOURCE_LOCATION);
 		for(int k=0; k<resources.length; k++) {
 			String [] tokens = resources[k].split("\\.");
@@ -170,7 +170,7 @@ public class TestIdProcessing {
 	@AfterClass
 	public static void removeDb() throws IOException {
 		File dir = new File(TEST_DB_LOCATION);
-		//FileUtils.deleteDirectory(dir);
+		FileUtils.deleteDirectory(dir);
 		System.out.println("\ndeleting typed obj database");
 	}
 	
@@ -212,7 +212,19 @@ public class TestIdProcessing {
 		// check that all expected Ids are in fact found
 		String [] foundIdRefs = report.getListOfIdReferences();
 		for(int k=0; k<foundIdRefs.length; k++) {
-			assertTrue("  -("+instance.resourceName+") extracted key "+foundIdRefs[k]+" that should not have been extracted",expectedIdList.containsKey(foundIdRefs[k]));
+			assertTrue("  -("+instance.resourceName+") extracted id "+foundIdRefs[k]+" that should not have been extracted",
+					expectedIdList.containsKey(foundIdRefs[k]));
+			int n_refs = expectedIdList.get(foundIdRefs[k]).intValue();
+			assertFalse("  -("+instance.resourceName+") extracted id "+foundIdRefs[k]+" too many times",
+					n_refs==0);
+			expectedIdList.put(foundIdRefs[k], new Integer(n_refs-1));
+		}
+		Iterator<Map.Entry<String,Integer>> mapIter = expectedIdList.entrySet().iterator();
+		while(mapIter.hasNext()) {
+			Map.Entry<String, Integer> pair = mapIter.next();
+			int n_refs_remaining = pair.getValue().intValue();
+			assertTrue("  -("+instance.resourceName+") needed to extract id '"+pair.getKey()+"' "+n_refs_remaining+" more times",
+					n_refs_remaining == 0);
 		}
 		
 		
