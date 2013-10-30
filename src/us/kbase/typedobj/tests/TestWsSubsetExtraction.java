f069281bcf5b379e15822e4b9f4fd7af9cc7d6f5
msneddon <mwsneddon@lbl.gov>
2013-10-30 16:37:10 -0700

    added test rig for extracting ws_subset from instances

---
+++ src/us/kbase/typedobj/tests/TestWsSubsetExtraction.java
@@ -0,0 +1,298 @@
+package us.kbase.typedobj.tests;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.diff.JsonDiff;
+
+import us.kbase.typedobj.core.TypeDefId;
+import us.kbase.typedobj.core.TypeDefName;
+import us.kbase.typedobj.core.TypedObjectValidationReport;
+import us.kbase.typedobj.core.TypedObjectValidator;
+import us.kbase.typedobj.db.FileTypeStorage;
+import us.kbase.typedobj.db.TypeDefinitionDB;
+import us.kbase.typedobj.db.UserInfoProviderForTests;
+import us.kbase.workspace.kbase.Util;
+
+
+/**
+ * Tests that ensure the proper subset is extracted from a typed object instance
+ *
+ * To add new tests of the ID processing machinery, add files named in the format:
+ *   [ModuleName].[TypeName].instance.[label] 
+ *        - json encoding of a valid type instance
+ *   [ModuleName].[TypeName].instance.[label].subset
+ *        - json encoding of the expected '\@searchable ws_subset' output
+ *
+ * @author msneddon
+ */
+@RunWith(value = Parameterized.class)
+public class TestWsSubsetExtraction {
+
+	/**
+	 * location to stash the temporary database for testing
+	 * WARNING: THIS DIRECTORY WILL BE WIPED OUT AFTER TESTS!!!!
+	 */
+	private final static String TEST_DB_LOCATION = "test/typedobj_temp_test_files/t3";
+	
+	/**
+	 * relative location to find the input files
+	 */
+	private final static String TEST_RESOURCE_LOCATION = "files/t3/";
+	
+	private final static boolean VERBOSE = true;
+
+	private static TypeDefinitionDB db;
+	private static TypedObjectValidator validator;
+	
+	/**
+	 * List to stash the handle to the test case files
+	 */
+	private static List<TestInstanceInfo> instanceResources = new ArrayList <TestInstanceInfo> ();
+	
+	private static class TestInstanceInfo {
+		public TestInstanceInfo(String resourceName, String moduleName, String typeName) {
+			this.resourceName = resourceName;
+			this.moduleName = moduleName;
+			this.typeName = typeName;
+		}
+		public String resourceName;
+		public String moduleName;
+		public String typeName;
+	}
+	
+	/**
+	 * As each test instance object is created, this sets which instance to actually test
+	 */
+	private TestInstanceInfo instance;
+	
+	public TestWsSubsetExtraction(TestInstanceInfo tii) {
+		this.instance = tii;
+	}
+
+	/**
+	 * This is invoked before anything else, so here we invoke the creation of the db
+	 * @return
+	 * @throws Exception 
+	 */
+	@Parameters
+	public static Collection<Object[]> assembleTestInstanceList() throws Exception {
+		prepareDb();
+		Object [][] instanceInfo = new Object[instanceResources.size()][1];
+		for(int k=0; k<instanceResources.size(); k++) {
+			instanceInfo[k][0] = instanceResources.get(k);
+		}
+		
+		return Arrays.asList(instanceInfo);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Setup the typedef database, load and release the types in the simple specs, and
+	 * identify all the files containing instances to validate.
+	 * @throws Exception
+	 */
+	public static void prepareDb() throws Exception
+	{
+		
+		//ensure test location is available
+		File dir = new File(TEST_DB_LOCATION);
+		if (dir.exists()) {
+			//fail("database at location: "+TEST_DB_LOCATION+" already exists, remove/rename this directory first");
+			removeDb();
+		}
+		if(!dir.mkdirs()) {
+			fail("unable to create needed test directory: "+TEST_DB_LOCATION);
+		}
+		
+		if(VERBOSE) System.out.println("setting up the typed obj database at '"+dir.getAbsolutePath()+"'");
+		// point the type definition db to point there
+		File tempdir = new File("temp_files");
+		if (!dir.exists())
+			dir.mkdir();
+		db = new TypeDefinitionDB(new FileTypeStorage(TEST_DB_LOCATION), tempdir, new UserInfoProviderForTests(),new Util().getKIDLpath());
+		
+		
+		// create a validator that uses the type def db
+		validator = new TypedObjectValidator(db);
+	
+		String username = "wstester1";
+		
+		String kbSpec = loadResourceFile(TEST_RESOURCE_LOCATION+"KB.spec");
+		List<String> kb_types =  Arrays.asList("SimpleStructure");
+		db.requestModuleRegistration("KB", username);
+		db.approveModuleRegistrationRequest(username, "KB");
+		db.registerModule(kbSpec ,kb_types, username);
+		db.releaseModule("KB", username);
+		
+		if(VERBOSE) System.out.print("finding test instances...");
+		String [] resources = getResourceListing(TEST_RESOURCE_LOCATION);
+		for(int k=0; k<resources.length; k++) {
+			String [] tokens = resources[k].split("\\.");
+			if(tokens.length!=4) { continue; }
+			if(tokens[2].equals("instance")) {
+				instanceResources.add(new TestInstanceInfo(resources[k],tokens[0],tokens[1]));
+			}
+		}
+		if(VERBOSE) System.out.println(" " + instanceResources.size() + "found");
+	}
+	
+	
+	@AfterClass
+	public static void removeDb() throws IOException {
+		File dir = new File(TEST_DB_LOCATION);
+		FileUtils.deleteDirectory(dir);
+		if(VERBOSE) System.out.println("deleting typed obj database");
+	}
+	
+	@Test
+	public void testValidInstances() throws Exception
+	{
+		ObjectMapper mapper = new ObjectMapper();
+		
+		//read the instance data
+		if(VERBOSE) System.out.println("  -("+instance.resourceName+")");
+		String instanceJson = loadResourceFile(TEST_RESOURCE_LOCATION+instance.resourceName);
+		JsonNode instanceRootNode = mapper.readTree(instanceJson);
+		
+		// read the ids file, which provides the list of ids we expect to extract from the instance
+		String expectedSubsetString = loadResourceFile(TEST_RESOURCE_LOCATION+instance.resourceName+".subset");
+		JsonNode expectedSubset = mapper.readTree(expectedSubsetString);
+		
+		// perform the initial validation, which must validate!
+		TypedObjectValidationReport report = 
+			validator.validate(
+				instanceRootNode,
+				new TypeDefId(new TypeDefName(instance.moduleName,instance.typeName))
+				);
+		List <String> mssgs = report.getErrorMessagesAsList();
+		for(int i=0; i<mssgs.size(); i++) {
+			System.out.println("    ["+i+"]:"+mssgs.get(i));
+		}
+		assertTrue("  -("+instance.resourceName+") does not validate, but should",
+				report.isInstanceValid());
+		
+		JsonNode actualSubset = report.extractSearchableWsSubset();
+		// we can just check if they are equal like so:
+		//assertTrue("  -("+instance.resourceName+") extracted subset does not match expected extracted subset",
+		//		actualSubset.equals(expectedSubset));
+		// this method generates a patch, so that if they differ you can see what's up
+		JsonNode diff = JsonDiff.asJson(actualSubset,expectedSubset);
+		if(VERBOSE) if(diff.size()!=0) System.out.println("      FAIL: diff:"+diff);
+		assertTrue("  -("+instance.resourceName+") extracted subset does not match expected extracted subset; diff="+diff,
+						diff.size()==0);
+		
+	}
+
+	/**
+	 * helper method to load test files, mostly copied from TypeRegistering test
+	 */
+	private static String loadResourceFile(String resourceName) throws Exception {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		InputStream is = TestWsSubsetExtraction.class.getResourceAsStream(resourceName);
+		if (is == null)
+			throw new IllegalStateException("Resource not found: " + resourceName);
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		while (true) {
+			String line = br.readLine();
+			if (line == null)
+				break;
+			pw.println(line);
+		}
+		br.close();
+		pw.close();
+		return sw.toString();
+	}
+	
+	
+	
+	/**
+	 * List directory contents for a resource folder. Not recursive.
+	 * This is basically a brute-force implementation.
+	 * Works for regular files and also JARs.
+	 * adapted from: http://www.uofr.net/~greg/java/get-resource-listing.html
+	 * 
+	 * @author Greg Briggs
+	 * @author msneddon
+	 * @param path Should end with "/", but not start with one.
+	 * @return Just the name of each member item, not the full paths.
+	 * @throws URISyntaxException 
+	 * @throws IOException 
+	 */
+	private static String[] getResourceListing(String path) throws URISyntaxException, IOException {
+		URL dirURL = TestWsSubsetExtraction.class.getResource(path);
+		if (dirURL != null && dirURL.getProtocol().equals("file")) {
+			/* A file path: easy enough */
+			return new File(dirURL.toURI()).list();
+		}
+
+		if (dirURL == null) {
+			// In case of a jar file, we can't actually find a directory.
+			// Have to assume the same jar as the class.
+			String me = TestWsSubsetExtraction.class.getName().replace(".", "/")+".class";
+			dirURL = TestWsSubsetExtraction.class.getResource(me);
+		}
+
+		if (dirURL.getProtocol().equals("jar")) {
+			/* A JAR path */
+			String jarPath = dirURL.getPath().substring(5, dirURL.getPath().indexOf("!")); //strip out only the JAR file
+			JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
+			Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+			Set<String> result = new HashSet<String>(); //avoid duplicates in case it is a subdirectory
+			while(entries.hasMoreElements()) {
+				String name = entries.nextElement().getName();
+				// construct internal jar path relative to the class
+				String fullPath = TestWsSubsetExtraction.class.getPackage().getName().replace(".","/") + "/" + path;
+				if (name.startsWith(fullPath)) { //filter according to the path
+					String entry = name.substring(fullPath.length());
+					int checkSubdir = entry.indexOf("/");
+					if (checkSubdir >= 0) {
+						// if it is a subdirectory, we just return the directory name
+						entry = entry.substring(0, checkSubdir);
+					}
+					result.add(entry);
+				}
+			}
+			jar.close();
+			return result.toArray(new String[result.size()]);
+		}
+		throw new UnsupportedOperationException("Cannot list files for URL "+dirURL);
+	}
+	
+}
