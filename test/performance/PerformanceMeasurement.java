b385a59ae3f6a43e18b15a62147e5852884be5e2
Gavin Price <gaprice@lbl.gov>
2014-03-14 15:00:03 -0700

    WIP on peformance testing

---
+++ test/performance/PerformanceMeasurement.java
@@ -0,0 +1,69 @@
+package performance;
+
+import java.util.List;
+
+/** Simple class to record performance of a set of reads and writes.
+ * 
+ * @author gaprice@lbl.gov
+ *
+ */
+public class PerformanceMeasurement {
+	
+	private final static double nanoToSec = 1000000000.0;
+	
+	private final double wMean;
+	private final double wStd;
+	private final double rMean;
+	private final double rStd;
+	
+	
+	/** Constructor
+	 * @param writes - a set of writes in ns.
+	 * @param reads - a set of reads in ns.
+	 */
+	public PerformanceMeasurement(final List<Long> writes,
+			final List<Long> reads) {
+		this.wMean = mean(writes);
+		this.rMean = mean(reads);
+		this.wStd = stddev(wMean, writes, false);
+		this.rStd = stddev(rMean, writes, false);
+	}
+	
+	public double getAverageWritesInSec() {
+		return wMean / nanoToSec;
+	}
+	
+	public double getAverageReadsInSec() {
+		return rMean / nanoToSec;
+	}
+	
+	public static double mean(final List<Long> nums) {
+		double sum = 0;
+		for (Long n: nums) {
+			sum += n;
+		}
+		return sum / nums.size();
+	}
+	
+	public double getStdDevWritesInSec() {
+		return wStd / nanoToSec;
+	}
+	
+	public double getStdDevReadsInSec() {
+		return rStd / nanoToSec;
+	}
+	
+	public static double stddev(final double mean, final List<Long> values,
+			final boolean population) {
+		if (values.size() < 2) {
+			return Double.NaN;
+		}
+		final double pop = population ? 0 : -1;
+		double accum = 0;
+		for (Long d: values) {
+			accum += Math.pow(new Double(d) - mean, 2);
+		}
+		return Math.sqrt(accum / (values.size() + pop));
+	}
+	
+}
