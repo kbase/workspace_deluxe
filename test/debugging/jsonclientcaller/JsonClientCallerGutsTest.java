caa051e3f3fbd7e459221d113295561b615a1fcc
Gavin Price <gaprice@lbl.gov>
2014-10-17 13:12:34 -0700

    Minimize the tests

--- test/debugging/jsonclientcaller/JsonClientCallerGutsTest.java
+++ test/debugging/jsonclientcaller/JsonClientCallerGutsTest.java
@@ -1,49 +1,34 @@
 package debugging.jsonclientcaller;
 
-import static debugging.jsonclientcaller.JsonClientCaller.streamToString;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import us.kbase.abstracthandle.AbstractHandleClient;
-import us.kbase.auth.AuthService;
-import us.kbase.auth.AuthToken;
 
 
 public class JsonClientCallerGutsTest {
 	private static final String URL = "http://dev03.berkeley.kbase.us:7109";
 	private static final int SLEEP = 1000; //ms between requests
+	private static final int COUNT = 10;
 	
 
 	public static void main(String [] args) throws Exception{
 		System.out.println(URL);
-		String user = args[0];
-		String pwd = args[1];
-		int count = Integer.parseInt(args[2]);
 		int excepts = 0;
-		for (int i = 0; i < count; i++) {
+		for (int i = 0; i < COUNT; i++) {
 			System.out.print(i + "\n");
 			try {
 				HttpURLConnection conn =
 						(HttpURLConnection) new URL(URL).openConnection();
 				conn.setDoOutput(true);
 				conn.setRequestMethod("POST");
-				AuthToken accessToken = AuthService.login(
-						user, pwd).getToken();
-				conn.setRequestProperty("Authorization",
-						accessToken.toString());
 				final long[] sizeWrapper = new long[] {0};
 				OutputStream os = new OutputStream() {
 					@Override
@@ -53,8 +38,8 @@ public class JsonClientCallerGutsTest {
 					@Override
 					public void write(byte[] b, int o, int l) {sizeWrapper[0] += l;}
 				};
-				String method = "AbstractHandle.are_readable";
-				Object arg = Arrays.asList(Arrays.asList("KBH_3"));
+				String method = "AbstractHandle.list_handles";
+				Object arg = new ArrayList<Object>();
 				String id = "12345";
 				
 				writeRequestData(method, arg, os, id);
@@ -64,13 +49,7 @@ public class JsonClientCallerGutsTest {
 				writeRequestData(method, arg, conn.getOutputStream(), id);
 				System.out.print(conn.getResponseCode() + " ");
 				System.out.println(conn.getResponseMessage());
-				conn.getResponseMessage();
-				InputStream istream;
-				if (conn.getResponseCode() == 500) {
-					istream = conn.getErrorStream();
-				} else {
-					istream = conn.getInputStream();
-				}
+				InputStream istream = conn.getInputStream();
 				// Parse response into json
 				System.out.println(streamToString(istream));
 			} catch (Exception e) {
@@ -81,7 +60,7 @@ public class JsonClientCallerGutsTest {
 			Thread.sleep(SLEEP);
 		}
 		System.out.println(String.format("Sleep: %s, failures %s/%s",
-				SLEEP, excepts, count));
+				SLEEP, excepts, COUNT));
 	}
 
 	public static void writeRequestData(String method, Object arg,
@@ -97,5 +76,13 @@ public class JsonClientCallerGutsTest {
 		g.writeEndObject();
 		g.close();
 		os.flush();
+	}
+	
+	public static String streamToString(InputStream os) {
+		Scanner s = new Scanner(os);
+		s.useDelimiter("\\A");
+		String ret = s.next();
+		s.close();
+		return ret;
 	}
 }
