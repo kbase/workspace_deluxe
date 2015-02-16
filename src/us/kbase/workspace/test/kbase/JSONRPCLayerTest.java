454382e42ed8ff523bb12e432b9e58851f144081
Gavin Price <gaprice@lbl.gov>
2015-02-15 17:39:18 -0800

    Update release notes and ver

--- src/us/kbase/workspace/test/kbase/JSONRPCLayerTest.java
+++ src/us/kbase/workspace/test/kbase/JSONRPCLayerTest.java
@@ -80,7 +80,7 @@ public class JSONRPCLayerTest extends JSONRPCLayerTester {
 	
 	@Test
 	public void ver() throws Exception {
-		assertThat("got correct version", CLIENT_NO_AUTH.ver(), is("0.3.3"));
+		assertThat("got correct version", CLIENT_NO_AUTH.ver(), is("0.3.4"));
 	}
 	
 	@Test
