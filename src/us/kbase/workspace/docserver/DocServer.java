a6b4f2f5f78d849da39a57929396baef6689fe25
Gavin Price <gaprice@lbl.gov>
2015-11-11 19:26:15 -0800

    Doc server works

--- src/us/kbase/workspace/docserver/DocServer.java
+++ src/us/kbase/workspace/docserver/DocServer.java
@@ -1,12 +1,14 @@
 package us.kbase.workspace.docserver;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.IOUtils;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -20,12 +22,23 @@ public class DocServer extends HttpServlet {
 			final HttpServletRequest request,
 			final HttpServletResponse response)
 			throws ServletException, IOException {
+		//TODO needs logging
 		
+		String path = request.getPathInfo();
+		if (path.equals("/")) {
+			path = "/index.html";
+		}
+		path = "" + path; //TODO add resources dir
+		System.out.println("path: " + path);
+		final InputStream is = getClass().getResourceAsStream(path);
+		if (is == null) {
+			response.sendError(404, path); //path here
+			return;
+		}
+		final byte[] page = IOUtils.toByteArray(is);
+		System.out.println("page length: " + page.length);
 		
-		response.getOutputStream().println(request.getPathInfo());
-		
-		
-		
+		response.getOutputStream().write(page);
 	}
 	
 	public void startupServer(int port) throws Exception {
