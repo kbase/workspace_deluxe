59b78fa50c807daa75f2d9dd1ae937cb81e5cfc6
Gavin Price <gaprice@lbl.gov>
2016-05-11 22:30:01 -0700

    Add DB schema versioning.

--- src/us/kbase/workspace/database/mongo/exceptions/BlobStoreException.java
+++ src/us/kbase/workspace/database/mongo/exceptions/BlobStoreException.java
@@ -1,11 +1,13 @@
 package us.kbase.workspace.database.mongo.exceptions;
 
+import us.kbase.workspace.database.exceptions.WorkspaceDBException;
+
 /** 
  * Thrown when a problem with the workspace blob store occurs.
  * @author gaprice@lbl.gov
  *
  */
-public class BlobStoreException extends Exception {
+public class BlobStoreException extends WorkspaceDBException {
 
 	private static final long serialVersionUID = 1L;
 	
