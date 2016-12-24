b0015335f3b7ce61b1a0e4fa76d0373145eb4f73
Gavin Price <gaprice@lbl.gov>
2016-12-24 10:45:37 -0800

    Update to shock script

--- performance/us/kbase/workspace/performance/workspace/get_from_shock.py
+++ performance/us/kbase/workspace/performance/workspace/get_from_shock.py
@@ -15,6 +15,7 @@ def main():
     db = mcli[MONGO_DB]
     ws = -1
     md5s = []
+    md5start = time.clock()
     for ver in db.workspaceObjVersions.find().sort([('ws', 1), ('id', 1)]):
         if ws == -1:
             ws = ver['ws']
@@ -22,20 +23,28 @@ def main():
             if ws != ver['ws']:
                 raise ValueError('more than one workspace found')
         md5s.append(ver['chksum'])
+    md5end = time.clock()
     nodes = []
     for md5 in md5s:
         rec = db.shock_nodeMap.find_one({'chksum': md5})
         nodes.append(rec['node'])
-    print nodes
+    nodesend = time.clock()
     headers = {'Authorization': 'OAuth ' + token}
     times = []
     count = 1
     for node in nodes:
         now = time.clock()
-        ret = requests.get(SHOCK_HOST + '/node/' + node + '/?download', headers=headers).text
+        ret = requests.get(SHOCK_HOST + '/node/' + node + '/?download', headers=headers)
+        ret.text
         times.append(time.clock() - now)
-        print count
+        if ret.status_code != 200:
+            raise ValueError('Non 200 return code')
+        if count % 10000 == 0:
+            print count
         count += 1
+    print 'Time to pull md5s from workspace obj vers: ' + str(md5end - md5start)
+    print 'Time to pull nodes from shock node map: ' + str(nodesend - md5end)
+    print 'Time to pull records from shock: ' + str(time.clock() - nodesend)
     print 'N: ' + str(len(times))
     mean = sum(times) / float(len(times))
     print 'Mean: ' + str(mean)
