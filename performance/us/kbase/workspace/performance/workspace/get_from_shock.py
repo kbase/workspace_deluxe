5153c457663c4109b78554becd7b1749875fa9db
Gavin Price <gaprice@lbl.gov>
2017-01-04 17:07:22 -0800

    Fix timing numbers
    Was using cpu elapsed vs. wall time

--- performance/us/kbase/workspace/performance/workspace/get_from_shock.py
+++ performance/us/kbase/workspace/performance/workspace/get_from_shock.py
@@ -24,7 +24,7 @@ def main():
     db = mcli[MONGO_DB]
     ws = -1
     md5s = []
-    md5start = time.clock()
+    md5start = time.time()
     for ver in db.workspaceObjVersions.find().sort([('ws', 1), ('id', 1)]):
         if ws == -1:
             ws = ver['ws']
@@ -32,19 +32,19 @@ def main():
             if ws != ver['ws']:
                 raise ValueError('more than one workspace found')
         md5s.append(ver['chksum'])
-    md5end = time.clock()
+    md5end = time.time()
     nodes = []
     for md5 in md5s:
         rec = db.shock_nodeMap.find_one({'chksum': md5})
         nodes.append(rec['node'])
-    nodesend = time.clock()
+    nodesend = time.time()
     headers = {'Authorization': 'OAuth ' + token}
     times = []
     count = 1
     fnull = open(os.devnull, 'w')
     for node in nodes:
         url = SHOCK_HOST + '/node/' + node + '/?download'
-        now = time.clock()
+        now = time.time()
         if use_curl:
             subprocess.check_call(['curl', '-H', 'Authorization: OAuth ' + token, url],
                                   stdout=fnull, stderr=subprocess.STDOUT)
@@ -53,14 +53,14 @@ def main():
             ret.text
             if ret.status_code != 200:
                 raise ValueError('Non 200 return code')
-        times.append(time.clock() - now)
+        times.append(time.time() - now)
         if count % 10000 == 0:
             print count
         count += 1
     fnull.close()
     print 'Time to pull md5s from workspace obj vers: ' + str(md5end - md5start)
     print 'Time to pull nodes from shock node map: ' + str(nodesend - md5end)
-    print 'Time to pull records from shock: ' + str(time.clock() - nodesend)
+    print 'Time to pull records from shock: ' + str(time.time() - nodesend)
     print 'N: ' + str(len(times))
     mean = sum(times) / float(len(times))
     print 'Mean: ' + str(mean)
