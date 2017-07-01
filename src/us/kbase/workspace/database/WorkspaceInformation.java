501a3e5510be8d0f6530cc0023104cce763dfe15
Gavin Price <gaprice@lbl.gov>
2017-06-30 17:01:04 -0700

    Add javadoc to WorkspaceInformation

--- src/us/kbase/workspace/database/WorkspaceInformation.java
+++ src/us/kbase/workspace/database/WorkspaceInformation.java
@@ -5,15 +5,21 @@ import static us.kbase.common.utils.StringUtils.checkString;
 
 import java.time.Instant;
 
+/** A value class containing various information about a workspace, including the id, owner,
+ * and other parameters. Note that this class is immutable and cannot not update its state once
+ * created, which means it will become out of date as the workspace storage system is changed.
+ * @author gaprice@lbl.gov
+ *
+ */
 public class WorkspaceInformation {
 	
 	//TODO CODE add is deleted.
-	//TODO NOW JAVADOC
 	//TODO NOW TEST
 	
-	public static final String UNLOCKED = "unlocked";
-	public static final String LOCKED = "locked";
-	public static final String PUBLISHED = "published";
+	//enum for this?
+	private static final String UNLOCKED = "unlocked";
+	private static final String LOCKED = "locked";
+	private static final String PUBLISHED = "published";
 	
 	final private long id;
 	final private String name;
@@ -46,42 +52,81 @@ public class WorkspaceInformation {
 		this.usermeta = meta;
 	}
 
+	/** Get the workspace's ID.
+	 * @return the ID.
+	 */
 	public long getId() {
 		return id;
 	}
 
+	/** Get the workspace's name.
+	 * @return the name.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/** Get the workspace owner.
+	 * @return the owner.
+	 */
 	public WorkspaceUser getOwner() {
 		return owner;
 	}
 
+	/** Get the date the workspace was last modified.
+	 * @return the modification date.
+	 */
 	public Instant getModDate() {
 		return modDate;
 	}
 
+	/** Get the ID of the last object that was created in the workspace. Note that the object
+	 * corresponding to this ID may have been deleted.
+	 * @return the largest ID that has ever existed in the workspace.
+	 */
 	public long getMaximumObjectID() {
 		return maxObjectID;
 	}
 
+	// this doesn't really make sense to keep in the workspace information...? not sure.
+	/** Get the permission the user that requested this workspace information possesses.
+	 * The user may or may not be the same as the owner.
+	 * @return the user's permission. 
+	 */
 	public Permission getUserPermission() {
 		return userPermission;
 	}
 
+	/** Get whether all users can read the workspace.
+	 * @return true if the workspace is globally readable.
+	 */
 	public boolean isGloballyReadable() {
 		return globalRead;
 	}
 	
+	/** Get whether the workspace is locked and cannot be altered.
+	 * @return true if the workspace is locked.
+	 */
 	public boolean isLocked() {
 		return locked;
 	}
 
+	/** Get any metadata associated with the workspace.
+	 * @return the workspace metadata.
+	 */
 	public UncheckedUserMetadata getUserMeta() {
 		return usermeta;
 	}
 	
+	/** Returns a string reflecting the lock state of the workspace. One of:
+	 * <ul>
+	 * <li>unlocked - the workspace is not locked and may be altered.</li>
+	 * <li>locked - the workspace is locked and cannot be altered other than to make it globally
+	 * readable.</li>
+	 * <li>published - the workspace is locked and globally readable and cannot be altered.</li>
+	 * </ul>
+	 * @return the lock state of the workspace.
+	 */
 	public String getLockState() {
 		if (!locked) {
 			return UNLOCKED;
@@ -191,12 +236,19 @@ public class WorkspaceInformation {
 		return true;
 	}
 	
-	// note required info
+	/** Get a builder for a workspace information instance. Note that several builder arguments
+	 * are required and the {@link Builder#build()} method will fail if they are not set.
+	 * @return a new builder.
+	 */
 	public static Builder getBuilder() {
 		return new Builder();
 	}
 	
-	// note required info
+	/** A builder for a workspace information instance. Note that several builder arguments
+	 * are required and the {@link Builder#build()} method will fail if they are not set.
+	 * @author gaprice@lbl.gov
+	 *
+	 */
 	public static class Builder {
 		
 		// required
@@ -227,7 +279,10 @@ public class WorkspaceInformation {
 					globalRead, locked, usermeta);
 		}
 		
-		// required
+		/** Set the workspace's ID (required).
+		 * @param id the ID.
+		 * @return this builder.
+		 */
 		public Builder withID(final long id) {
 			if (id < 1) {
 				throw new IllegalArgumentException("id must be > 0");
@@ -236,28 +291,43 @@ public class WorkspaceInformation {
 			return this;
 		}
 		
-		//required
+		/** Set the workspace's name (required).
+		 * Note that other than a null and empty string check, this value is not validated as
+		 * per {@link WorkspaceIdentifier#checkWorkspaceName(String, WorkspaceUser)} since this
+		 * class is expected to be created based on values from a database rather than user input.
+		 * @param name the name.
+		 * @return this builder.
+		 */
 		public Builder withName(final String name) {
 			checkString(name, "name");
 			this.name = name;
 			return this;
 		}
 		
-		//required
+		/** Set the workspace's owner (required).
+		 * @param owner the owner.
+		 * @return this builder.
+		 */
 		public Builder withOwner(final WorkspaceUser owner) {
 			nonNull(owner, "owner");
 			this.owner = owner;
 			return this;
 		}
 		
-		//required
+		/** Set the last modification date for the workspace (required).
+		 * @param modDate the last modification date.
+		 * @return this builder.
+		 */
 		public Builder withModificationDate(final Instant modDate) {
 			nonNull(modDate, "modDate");
 			this.modDate = modDate;
 			return this;
 		}
 		
-		//required
+		/** Set the maximum object ID that exists or has existed in the workspace (required).
+		 * @param maxObjectID the maximum object ID.
+		 * @return this builder.
+		 */
 		public Builder withMaximumObjectID(final long maxObjectID) {
 			if (maxObjectID < 0) {
 				throw new IllegalArgumentException("max id must be at least 0");
@@ -266,23 +336,38 @@ public class WorkspaceInformation {
 			return this;
 		}
 		
-		// required
+		/** Set the permission of the user that requested this workspace information (required).
+		 * @param perm the user's permission.
+		 * @return this builder.
+		 */
 		public Builder withUserPermission(final Permission perm) {
 			nonNull(perm, "perm");
 			this.userPermission = perm;
 			return this;
 		}
 		
+		/** Set whether this workspace is globally readable (default false).
+		 * @param globalRead true to set the workspace as globally readable.
+		 * @return this builder.
+		 */
 		public Builder withGlobalRead(final boolean globalRead) {
 			this.globalRead = globalRead;
 			return this;
 		}
 		
+		/** Set whether this workspace is locked (default false).
+		 * @param locked true to set the workspace as locked.
+		 * @return this builder.
+		 */
 		public Builder withLocked(final boolean locked) {
 			this.locked = locked;
 			return this;
 		}
 		
+		/** Set the metadata associated with the workspace.
+		 * @param meta the metadata.
+		 * @return this builder.
+		 */
 		public Builder withUserMetadata(final UncheckedUserMetadata meta) {
 			nonNull(meta, "meta");
 			this.usermeta = meta;
