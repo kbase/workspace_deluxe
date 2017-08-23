98720efa54bc2e88040a6f23afe387bb454c647f
Gavin Price <gaprice@lbl.gov>
2017-08-22 20:46:35 -0700

    Bump version

--- src/us/kbase/workspace/test/kbase/JSONRPCLayerTest.java
+++ src/us/kbase/workspace/test/kbase/JSONRPCLayerTest.java
@@ -92,7 +92,7 @@ public class JSONRPCLayerTest extends JSONRPCLayerTester {
 	
 	@Test
 	public void ver() throws Exception {
-		assertThat("got correct version", CLIENT_NO_AUTH.ver(), is("0.7.2-dev1"));
+		assertThat("got correct version", CLIENT_NO_AUTH.ver(), is("0.8.0-dev1"));
 	}
 	
 	public void status() throws Exception {
