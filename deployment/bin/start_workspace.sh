3a57a0aa7fb4866ead535fba1688398c576db2d8
Steve Chan <sychan168@gmail.com>
2017-11-03 17:48:56 -0700

    Updated to support new base image using dockerize entrypoint

---
+++ deployment/bin/start_workspace.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+cd /kb/deployment
+
+bin/glassfish_administer_service.py --verbose --admin $GLASSFISH/bin/asadmin --domain Workspace \
+        --domain-dir /kb/deployment/services/workspace/glassfish_domain \
+        --war /kb/deployment/services/workspace/WorkspaceService.war --port 7058 --threads 20 \
+        --Xms 10000 --Xmx 15000 --properties KB_DEPLOYMENT_CONFIG=$KB_DEPLOYMENT_CONFIG
