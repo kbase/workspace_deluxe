d8445ca774c166080c02c703830495cf4bba2b39
Steve Chan <sychan168@gmail.com>
2017-11-06 16:34:00 -0800

    Tail the log file if server starts successfully

--- deployment/bin/start_workspace.sh
+++ deployment/bin/start_workspace.sh
@@ -4,4 +4,5 @@ cd /kb/deployment
 bin/glassfish_administer_service.py --verbose --admin $GLASSFISH/bin/asadmin --domain Workspace \
         --domain-dir /kb/deployment/services/workspace/glassfish_domain \
         --war /kb/deployment/services/workspace/WorkspaceService.war --port 7058 --threads 20 \
-        --Xms 10000 --Xmx 15000 --properties KB_DEPLOYMENT_CONFIG=$KB_DEPLOYMENT_CONFIG
+        --Xms 10000 --Xmx 15000 --properties KB_DEPLOYMENT_CONFIG=$KB_DEPLOYMENT_CONFIG  && \
+tail -f /kb/deployment/services/workspace/glassfish_domain/Workspace/logs/server.log
