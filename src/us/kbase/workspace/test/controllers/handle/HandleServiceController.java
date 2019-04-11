4af23839a84655975709cd5e0cc8bddad2ddc8f9
Tianhao-Gu <tgu@anl.gov>
2019-04-11 08:51:42 -0500

    using python3

--- src/us/kbase/workspace/test/controllers/handle/HandleServiceController.java
+++ src/us/kbase/workspace/test/controllers/handle/HandleServiceController.java
@@ -108,22 +108,27 @@ public class HandleServiceController {
 					biokbase_dir.resolve(file_name).toFile());
 		}
 		
-		Path log_file = biokbase_dir.resolve("log.py");
-		Stream <String> lines = Files.lines(log_file);
-		List <String> replaced = lines.map(line -> line.replaceAll("import urllib.request as _urllib", 
+		Path log_file = null;
+		Stream <String> lines = null;
+		List <String> replaced = null;
+		
+		log_file = biokbase_dir.resolve("log.py");
+		lines = Files.lines(log_file);
+		replaced = lines.map(line -> line.replaceAll("from ConfigParser import ConfigParser as _ConfigParser", 
 				"try:\n" + 
-				"    import urllib.request as _urllib\n" + 
+				"    from ConfigParser import ConfigParser as _ConfigParser\n" + 
 				"except ImportError:\n" + 
-				"    import urllib as _urllib")).collect(Collectors.toList());
+				"    from configparser import ConfigParser as _ConfigParser")).collect(Collectors.toList());
 		Files.write(log_file, replaced);
 		lines.close();
 		
 		log_file = handle_dir.resolve("AbstractHandleServer.py");
 		lines = Files.lines(log_file);
-		replaced = lines.map(line -> line.replaceAll("str]", "unicode]")).collect(Collectors.toList());
+		replaced = lines.map(line -> line.replaceAll("loads\\(request_body\\)",
+				"loads(request_body.decode('utf-8'))")).collect(Collectors.toList());
 		Files.write(log_file, replaced);
 		lines.close();
-		
+
 		log_file = handle_utils_dir.resolve("MongoUtil.py");
 		lines = Files.lines(log_file);
 		replaced = lines.map(line -> line.replaceAll("#print", 
