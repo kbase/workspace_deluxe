ac18db89c9f8a96b8c091eb5013536def66f1c43
Gavin Price <gaprice@lbl.gov>
2019-06-18 14:41:33 -0700

    Integrate S3 backend into workspace startup

--- src/us/kbase/workspace/kbase/InitConstants.java
+++ src/us/kbase/workspace/kbase/InitConstants.java
@@ -3,4 +3,5 @@ package us.kbase.workspace.kbase;
 public class InitConstants {
 	
 	public static final String COL_SHOCK_NODES = "shock_nodeMap";
+	public static final String COL_S3_OBJECTS = "s3_objects";
 }
