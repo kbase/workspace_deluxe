f81ced000c36f088a563cabcafea35b8b005707d
Gavin <gaprice@lbl.gov>
2022-04-22 23:07:59 -0700

    Simplify type provider interface
    The absolute type def and the jsonschema can be fetched in one call to
    the workspace, so make the provider a single call.

--- src/us/kbase/typedobj/core/TypeProvider.java
+++ src/us/kbase/typedobj/core/TypeProvider.java
@@ -1,39 +1,84 @@
 package us.kbase.typedobj.core;
 
+import static java.util.Objects.requireNonNull;
+
 import us.kbase.typedobj.exceptions.NoSuchModuleException;
 import us.kbase.typedobj.exceptions.NoSuchTypeException;
 import us.kbase.typedobj.exceptions.TypeStorageException;
 
-/** Provides type definitions to the TypedObjectValidator.
- * 
- * @author gaprice@lbl.gov
- *
- */
+/** Provides type definitions to the TypedObjectValidator. */
 public interface TypeProvider {
 
-	/** Translates a potentially unresolved type id (e.g. missing a minor
-	 * or major version) to an absolute type id.
-	 * @param typeDefId a type id that may not be completely specified.
-	 * @return a completely specified type id.
-	 * @throws TypeStorageException if an error occurs with the type storage
-	 * engine
-	 * @throws NoSuchModuleException if the module for the type does not exit
-	 * @throws NoSuchTypeException if the type does not exist
-	 */
-	public AbsoluteTypeDefId resolveTypeDef(final TypeDefId typeDefId)
-			throws NoSuchTypeException, NoSuchModuleException,
-			TypeStorageException;
+	/** A resolved type and its JSONSchema. */
+	public static class ResolvedType {
+		private final AbsoluteTypeDefId type;
+		private final String jsonSchema;
+
+		/** Construct the resolved type.
+		 * @param type the resolved type.
+		 * @param jsonSchema the JSONSchema.
+		 */
+		public ResolvedType(final AbsoluteTypeDefId type, final String jsonSchema) {
+			this.type = requireNonNull(type, "type");
+			this.jsonSchema = requireNonNull(jsonSchema, "jsonSchema");
+		}
+
+		/** Get the resolved type.
+		 * @return the type.
+		 */
+		public AbsoluteTypeDefId getType() {
+			return type;
+		}
+
+		/** Get the JSONSchema for the type.
+		 * @return the JSONSchema.
+		 */
+		public String getJsonSchema() {
+			return jsonSchema;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((jsonSchema == null) ? 0 : jsonSchema.hashCode());
+			result = prime * result + ((type == null) ? 0 : type.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ResolvedType other = (ResolvedType) obj;
+			if (jsonSchema == null) {
+				if (other.jsonSchema != null)
+					return false;
+			} else if (!jsonSchema.equals(other.jsonSchema))
+				return false;
+			if (type == null) {
+				if (other.type != null)
+					return false;
+			} else if (!type.equals(other.type))
+				return false;
+			return true;
+		}
+	}
 	
-	/** Retrieves the JsonSchema type definition document, as a string, for the
+	/** Retrieves the resolved type and JsonSchema type definition document for the
 	 * specified type.
-	 * @param typeDefId a type id.
-	 * @return the JsonSchema document for the type.
+	 * @param type a type id.
+	 * @return the resolved type with JSONSchema.
 	 * @throws TypeStorageException if an error occurs with the type storage
-	 * engine
-	 * @throws NoSuchModuleException if the module for the type does not exit
-	 * @throws NoSuchTypeException if the type does not exist
+	 * engine.
+	 * @throws NoSuchModuleException if the module for the type does not exist.
+	 * @throws NoSuchTypeException if the type does not exist.
 	 */
-	public String getTypeJsonSchema(final AbsoluteTypeDefId typeDefId)
+	public ResolvedType getTypeJsonSchema(final TypeDefId type)
 			throws NoSuchTypeException, NoSuchModuleException,
 			TypeStorageException;
 }
