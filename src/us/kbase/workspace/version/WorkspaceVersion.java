dc9cbb18c83e7e3fb1bbddfa26e8f214ef2f229b
Gavin <gaprice@lbl.gov>
2022-05-04 12:58:40 -0700

    Add Delegating Type Provider
    for the type validator.

--- src/us/kbase/workspace/version/WorkspaceVersion.java
+++ src/us/kbase/workspace/version/WorkspaceVersion.java
@@ -3,6 +3,8 @@ package us.kbase.workspace.version;
 /** The version of the workspace code. */
 public class WorkspaceVersion {
 	
+	private WorkspaceVersion() {};
+	
 	/** The version. */
 	public static final String VERSION = "0.13.2-dev1";
 
