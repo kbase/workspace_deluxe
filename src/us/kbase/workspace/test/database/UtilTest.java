d6cf4e95beb13e54d198d3cf9d2098d33aacd9e9
ialarmedalien <ialarmedalien@gmail.com>
2022-10-27 11:36:29 -0700

    Improvements to the tests based on PR feedback

--- src/us/kbase/workspace/test/database/UtilTest.java
+++ src/us/kbase/workspace/test/database/UtilTest.java
@@ -20,93 +20,96 @@ public class UtilTest {
     public static final String UNEXP_EXC = "unexpected exception";
     public static final String INCORRECT_NULL_EMPTY = "incorrect null or empty";
     public static final String NULL_STRING = null;
-    public static final String OK_STRING = "some string of strings strung together";
+    public static final String OK_STRING = "some string of stingy stringy strings strung together";
+    public static final String STRING_WITH_SPACES = "\n\n\n   string \t  \n";
+    public static final String FUN_UNICODE_STRING = "❌❉ ⨍∪ℕ ᏬᏁᎥς๏๔є sтя⌽ηg ❉❓❗";
+
+    public static final String WHITESPACE = "\n\n         \t\t  \r\n   ";
     public static final Long NULL_LONG = null;
     public static final Long OK_LONG = (long) 123456;
 
     public static final String TYPE_NAME = "some type";
 
-    protected final List<String> emptyStrings = Arrays.asList(
+    private static final List<String> EMPTY_STRINGS = Arrays.asList(
             "",
             "   ",
             "\n",
-            "\n\n         \t\t  \r\n   ");
+            WHITESPACE);
 
-    protected final List<String> nonEmptyStrings = Arrays.asList(
+    private static final List<String> NON_EMPTY_STRINGS = Arrays.asList(
             OK_STRING,
+            "ab",
             "\n5\n6\n7\n8\n",
-            "  string ",
-            "\n\n\n   string \t  \n");
+            STRING_WITH_SPACES,
+            FUN_UNICODE_STRING);
 
-    protected final List<String> emptyStringsWithNull = Arrays.asList(
+    private static final List<String> EMPTY_STRINGS_WITH_NULL = Arrays.asList(
             "",
             "   ",
             NULL_STRING,
             "\n",
-            "\n\n         \t\t  \r\n   ");
+            WHITESPACE);
 
-    protected final List<String> nonEmptyStringsWithNull = Arrays.asList(
+    private static final List<String> NON_EMPTY_STRINGS_WITH_NULL = Arrays.asList(
             OK_STRING,
-            "\n5\n",
+            "ab",
+            "\n5\n6\n7\n8\n",
             NULL_STRING,
-            "  string ",
-            "\n\n\n   string \t  \n");
+            STRING_WITH_SPACES,
+            FUN_UNICODE_STRING);
 
     @Test
-    public void passXorNameId() throws Exception {
+    public void xorNameIdPass() throws Exception {
         Util.xorNameId(NULL_STRING, OK_LONG, TYPE_NAME);
         Util.xorNameId(OK_STRING, NULL_LONG, TYPE_NAME);
     }
 
     @Test
-    public void failXorNameId() throws Exception {
+    public void xorNameIdFail() throws Exception {
         try {
             Util.xorNameId(NULL_STRING, NULL_LONG, TYPE_NAME);
             fail(EXP_EXC);
         } catch (Exception got) {
-            TestCommon.assertExceptionCorrect(got, new IllegalArgumentException(String.format(
-                    "Must provide one and only one of %s name (was: %s) or id (was: %s)",
-                    TYPE_NAME, NULL_STRING, NULL_LONG)));
+            TestCommon.assertExceptionCorrect(got, new IllegalArgumentException(
+                    "Must provide one and only one of some type name (was: null) or id (was: null)"));
         }
 
         try {
-            Util.xorNameId(OK_STRING, OK_LONG, TYPE_NAME);
+            Util.xorNameId(OK_STRING, OK_LONG, "a different type");
             fail(EXP_EXC);
         } catch (Exception got) {
-            TestCommon.assertExceptionCorrect(got, new IllegalArgumentException(String.format(
-                    "Must provide one and only one of %s name (was: %s) or id (was: %s)",
-                    TYPE_NAME, OK_STRING, OK_LONG)));
+            TestCommon.assertExceptionCorrect(got, new IllegalArgumentException(
+                    "Must provide one and only one of a different type name (was: some string of stingy stringy strings strung together) or id (was: 123456)"));
         }
     }
 
     @Test
-    public void passIsNullOrEmpty() throws Exception {
-        for (String empty : emptyStrings) {
+    public void isNullOrEmptyPass() throws Exception {
+        for (String empty : EMPTY_STRINGS_WITH_NULL) {
             assertThat(INCORRECT_NULL_EMPTY, Util.isNullOrEmpty(empty), is(true));
         }
-        assertThat(INCORRECT_NULL_EMPTY, Util.isNullOrEmpty(NULL_STRING), is(true));
     }
 
     @Test
-    public void failIsNullOrEmpty() throws Exception {
-        for (String nullOrEmpty : nonEmptyStrings) {
+    public void isNullOrEmptyFail() throws Exception {
+        for (String nullOrEmpty : NON_EMPTY_STRINGS) {
             assertThat(INCORRECT_NULL_EMPTY, Util.isNullOrEmpty(nullOrEmpty), is(false));
         }
     }
 
     @Test
-    public void passNonNull() throws Exception {
-        for (String empty : emptyStrings) {
+    public void nonNullPass() throws Exception {
+        for (String empty : EMPTY_STRINGS) {
             Util.nonNull(empty, OK_STRING);
         }
 
-        for (String nonEmpty : nonEmptyStrings) {
+        for (String nonEmpty : NON_EMPTY_STRINGS) {
             Util.nonNull(nonEmpty, OK_STRING);
         }
     }
 
     @Test
-    public void failNonNull() throws Exception {
+    public void nonNullFail() throws Exception {
         try {
             Util.nonNull(null, OK_STRING);
             fail(EXP_EXC);
@@ -116,16 +119,16 @@ public class UtilTest {
     }
 
     @Test
-    public void passNoNulls() throws Exception {
-        Util.noNulls(nonEmptyStrings, TYPE_NAME);
-        Util.noNulls(emptyStrings, TYPE_NAME);
+    public void noNullsPass() throws Exception {
+        Util.noNulls(NON_EMPTY_STRINGS, TYPE_NAME);
+        Util.noNulls(EMPTY_STRINGS, TYPE_NAME);
     }
 
     @Test
-    public void failNoNulls() throws Exception {
+    public void noNullsFail() throws Exception {
         final List<List<String>> testList = new ArrayList<>();
-        testList.add(emptyStringsWithNull);
-        testList.add(nonEmptyStringsWithNull);
+        testList.add(EMPTY_STRINGS_WITH_NULL);
+        testList.add(NON_EMPTY_STRINGS_WITH_NULL);
 
         for (List<String> testListItem : testList) {
             try {
@@ -138,16 +141,16 @@ public class UtilTest {
     }
 
     @Test
-    public void passCheckNoNullsOrEmpties() throws Exception {
-        Util.checkNoNullsOrEmpties(nonEmptyStrings, TYPE_NAME);
+    public void checkNoNullsOrEmptiesPass() throws Exception {
+        Util.checkNoNullsOrEmpties(NON_EMPTY_STRINGS, TYPE_NAME);
     }
 
     @Test
-    public void failCheckNoNullsOrEmpties() throws Exception {
+    public void checkNoNullsOrEmptiesFail() throws Exception {
         final List<List<String>> testList = new ArrayList<>();
-        testList.add(emptyStrings);
-        testList.add(emptyStringsWithNull);
-        testList.add(nonEmptyStringsWithNull);
+        testList.add(EMPTY_STRINGS);
+        testList.add(EMPTY_STRINGS_WITH_NULL);
+        testList.add(NON_EMPTY_STRINGS_WITH_NULL);
 
         for (List<String> testListItem : testList) {
             try {
@@ -155,18 +158,18 @@ public class UtilTest {
                 fail(EXP_EXC);
             } catch (Exception got) {
                 TestCommon.assertExceptionCorrect(got, new IllegalArgumentException(
-                        "Null or whitespace only string in collection " + TYPE_NAME));
+                        "Null or whitespace only string in collection some type"));
             }
         }
     }
 
     @Test
-    public void passCheckString() throws Exception {
+    public void checkStringPass() throws Exception {
         final String INCORRECT_CHECKSTRING = "incorrect checkString";
         final Map<String, String> trimmedNonEmptyStrings = new HashMap<>();
-        for (String nonEmpty : nonEmptyStrings) {
-            trimmedNonEmptyStrings.put(nonEmpty, nonEmpty.trim());
-        }
+        trimmedNonEmptyStrings.put(OK_STRING, OK_STRING);
+        trimmedNonEmptyStrings.put("\n5\n6\n7\n8\n", "5\n6\n7\n8");
+        trimmedNonEmptyStrings.put(STRING_WITH_SPACES, "string");
 
         for (Map.Entry<String, String> mapElement : trimmedNonEmptyStrings.entrySet()) {
             assertThat(
@@ -175,7 +178,7 @@ public class UtilTest {
                     is(mapElement.getValue()));
         }
 
-        final int max = 100;
+        final int max = 53;
         for (Map.Entry<String, String> mapElement : trimmedNonEmptyStrings.entrySet()) {
             assertThat(
                     INCORRECT_CHECKSTRING,
@@ -191,31 +194,46 @@ public class UtilTest {
                     Util.checkString(mapElement.getKey(), TYPE_NAME, -20),
                     is(mapElement.getValue()));
         }
+
+        // FUN_UNICODE_STRING should be 25 codePoints long.
+        final int codePointCount = 25;
+        assertThat(
+                INCORRECT_CHECKSTRING,
+                Util.checkString(FUN_UNICODE_STRING, TYPE_NAME, codePointCount),
+                is(FUN_UNICODE_STRING));
+        // force failure by adding a character
+        try {
+            Util.checkString(FUN_UNICODE_STRING + "⛔", TYPE_NAME, codePointCount);
+            fail(EXP_EXC);
+        } catch (Exception got) {
+            TestCommon.assertExceptionCorrect(got, new IllegalArgumentException(
+                    "some type size greater than limit 25"));
+        }
     }
 
     @Test
-    public void failCheckStringNullWhitespace() throws Exception {
-        for (String emptyNullString : emptyStringsWithNull) {
+    public void checkStringFailNullWhitespace() throws Exception {
+        for (String emptyNullString : EMPTY_STRINGS_WITH_NULL) {
             try {
                 Util.checkString(emptyNullString, TYPE_NAME);
                 fail(EXP_EXC);
             } catch (Exception got) {
                 TestCommon.assertExceptionCorrect(got, new IllegalArgumentException(
-                        TYPE_NAME + " cannot be null or whitespace only"));
+                        "some type cannot be null or whitespace only"));
             }
         }
     }
 
     @Test
-    public void failCheckStringTooLong() throws Exception {
+    public void checkStringFailTooLong() throws Exception {
         final int max = 1;
-        for (String nonEmptyString : nonEmptyStrings) {
+        for (String nonEmptyString : NON_EMPTY_STRINGS) {
             try {
                 Util.checkString(nonEmptyString, TYPE_NAME, max);
                 fail(EXP_EXC);
             } catch (Exception got) {
                 TestCommon.assertExceptionCorrect(got, new IllegalArgumentException(
-                        TYPE_NAME + " size greater than limit " + max));
+                        "some type size greater than limit 1"));
             }
         }
     }
