262e1d81404edd7df163db69315443503af178dd
ialarmedalien <ialarmedalien@gmail.com>
2022-11-08 06:57:50 -0800

    Formatter

--- src/us/kbase/workspace/database/provenance/EventDate.java
+++ src/us/kbase/workspace/database/provenance/EventDate.java
@@ -18,27 +18,33 @@ public class EventDate {
 	 * yyyy-MM
 	 * yyyy-MM-dd
 	 *
-	 * It also ensures that months are in the range 00-12 and days are in the range 00-31.
+	 * It also ensures that months are in the range 00-12 and days are in the range
+	 * 00-31.
 	 */
-	public static final Pattern VALID_DATE_REGEX = Pattern.compile("^[12]\\d{3}(-(0[1-9]|1[0-2])(-(0[1-9]|[12]\\d|3[01]))?)?$");
+	public static final Pattern VALID_DATE_REGEX = Pattern.compile(
+			"^[12]\\d{3}(-(0[1-9]|1[0-2])(-(0[1-9]|[12]\\d|3[01]))?)?$");
 
 	private EventDate(
-		final String date,
-		final String event) {
+			final String date,
+			final String event) {
 		this.date = date;
 		this.event = event;
 	}
 
-	/** Gets the date, for example "2022-05-10".
-	 * Dates are returned in the format yyyy-MM-dd; if no data is available for the month
-	 * or day, the string is truncated to yyyy-MM or yyyy respectively.
+	/**
+	 * Gets the date, for example "2022-05-10".
+	 * Dates are returned in the format yyyy-MM-dd; if no data is available for the
+	 * month or day, the string is truncated to yyyy-MM or yyyy respectively.
+	 *
 	 * @return the date.
 	 */
 	public String getDate() {
 		return date;
 	}
 
-	/** Gets the event that occurred on the date in question, for example "updated".
+	/**
+	 * Gets the event that occurred on the date in question, for example "updated".
+	 *
 	 * @return the event.
 	 */
 	public String getEvent() {
@@ -62,11 +68,12 @@ public class EventDate {
 		return Objects.equals(date, other.date) && Objects.equals(event, other.event);
 	}
 
-	/** Gets a builder for an {@link EventDate}.
+	/**
+	 * Gets a builder for an {@link EventDate}.
 	 *
-	 * @param date        the date when the event occurred,
-	 *                    in the format yyyy-MM-dd, yyyy-MM, or yyyy.
-	 * @param event       the event that occurred on that date
+	 * @param date  the date when the event occurred,
+	 *              in the format yyyy-MM-dd, yyyy-MM, or yyyy.
+	 * @param event the event that occurred on that date
 	 * @return the builder.
 	 */
 	public static Builder getBuilder(final String date, final String event) {
@@ -85,7 +92,9 @@ public class EventDate {
 			this.date = Common.checkAgainstRegex(protoDate, VALID_DATE_REGEX, "date", false);
 		}
 
-		/** Builds the {@link EventDate}.
+		/**
+		 * Builds the {@link EventDate}.
+		 *
 		 * @return the event date.
 		 */
 		public EventDate build() {
