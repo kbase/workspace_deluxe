7a6d5a2079a8c32fe8e4b046adf87509ed0127ae
ialarmedalien <ialarmedalien@gmail.com>
2023-02-09 06:14:45 -0800

    Further tinkering for better debugging

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 #
-# script to run the python workspace_container_test.py locally or on GitHub Actions.
-# builds and mounts auth2, workspace, and mongo docker containers, and then calls
+# Script to run the python workspace_container_test.py locally or on GitHub Actions.
+# Builds and mounts auth2, workspace, and mongo docker containers, and then calls
 # the python script.
 #
-# see .github/workflows/test.yml for GitHub Actions implementation
+# See .github/workflows/test.yml for GitHub Actions implementation.
 
+# build images
 docker compose build
+build_exit_code=$?
+if [ $build_exit_code -ne 0 ]; then
+    echo "Error: docker compose build failed with exit code $build_exit_code"
+    exit $build_exit_code
+fi
+
+# bring up the containers
 docker compose up -d
-
 compose_up_exit_code=$?
-
 if [ $compose_up_exit_code -ne 0 ]; then
     echo "Error: docker-compose up command failed with exit code $compose_up_exit_code."
     exit $compose_up_exit_code
@@ -22,9 +28,9 @@ exit_code=666
 
 # limit the number of retries of the script
 while [ $counter -lt $max_retries ]; do
-# wait for the workspace container to start up
-# logs should stop being generated and the last line of the logs should be something like
-# <timestamp> INFO [main] org.apache.catalina.startup.Catalina.start Server startup in 3811 ms
+    # wait for the workspace container to start up
+    # logs should stop being generated and the last line of the logs should be something like
+    # <timestamp> INFO [main] org.apache.catalina.startup.Catalina.start Server startup in 3811 ms
     if docker logs -n 5 "workspace_deluxe-workspace-1" 2>&1 | grep -q -F -m 1 'org.apache.catalina.startup.Catalina.start Server startup in' > /dev/null; then
         # get the path to the 'scripts' directory and add it to the python execution path
         current_dir="$( dirname -- "$( readlink -f -- "$0"; )"; )"
