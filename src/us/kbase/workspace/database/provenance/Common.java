d46558b02ae466f41658745fb7f9f6a92704ff8b
ialarmedalien <ialarmedalien@gmail.com>
2023-02-15 11:25:57 -0800

    dedupeSimpleList returns null if the list is empty

--- src/us/kbase/workspace/database/provenance/Common.java
+++ src/us/kbase/workspace/database/provenance/Common.java
@@ -66,11 +66,11 @@ class Common {
 	 * @param <T>
 	 * @param list
 	 *                list of items to deduplicate
-	 * @return immutable deduplicated list
+	 * @return immutable deduplicated list or null if the list is null or empty
 	 */
 	static <T> List<T> dedupeSimpleList(final List<T> list) {
-		if (list == null) {
-			return list;
+		if (list == null || list.isEmpty()) {
+			return null;
 		}
 
 		final List<T> dedupedList = list.stream()
